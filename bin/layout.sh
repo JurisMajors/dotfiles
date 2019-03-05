5d39a725e3500deb3572e6722928eb278fd97757
JurisMajors <j.a.majors@student.tue.nl>
2019-03-05 15:47:58 +0100

    bazzinga

--- bin/layout.sh
+++ bin/layout.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 output=$(xrandr | grep 'HDMI-0 connected') 
-if [[ $output ]]; then
 
+if [[ $output ]]; then
     xrandr --output eDP-1-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1-1 --off --output HDMI-0 --primary --mode 1920x1080 --pos 1920x0 --rotate normal
 else
     xrandr --output eDP-1-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1-1 --off --output HDMI-0 --off
 fi
+echo $(feh --randomize --bg-fill ~/Wallpapers/*)
