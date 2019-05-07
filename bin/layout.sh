a100db966491f749d4fe7d7cebaa930470b115f6
Juris Majors <j.a.majors@student.tue.nl>
2019-05-07 00:41:16 +0000

    desktop layout

--- bin/layout.sh
+++ bin/layout.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
-output=$(xrandr | grep 'HDMI-0 connected') 
+output=$(xrandr | grep 'DVI-D-0 connected') 
 
 if [[ $output ]]; then
-    xrandr --output eDP-1-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1-1 --off --output HDMI-0 --primary --mode 1920x1080 --pos 1920x0 --rotate normal
-else
-    xrandr --output eDP-1-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1-1 --off --output HDMI-0 --off
+    xrandr --output DVI-D-0 --mode 1920x1080 --rate 144 --primary
 fi
 echo $(feh --randomize --bg-fill ~/Wallpapers/)
