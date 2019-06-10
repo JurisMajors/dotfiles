bd55ce9fa262e8ee28f4fb41c92a56b55c2023ba
Juris Majors <j.a.majors@student.tue.nl>
2019-06-10 13:50:35 +0200

    added highlight to slop selection

--- bin/screenshot.sh
+++ bin/screenshot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-maim -s --format png /dev/stdout | xclip -selection clipboard -t image/png -i
+maim --highlight --color=0.3,0.4,0.6,0.4 -s --format png /dev/stdout | xclip -selection clipboard -t image/png -i
 notify-send -u "low" -t 1000 "Copied screenshot on clipboard!"
