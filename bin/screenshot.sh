81a9a8e54a697b718fd3bedaadb98c0106f98cd1
Juris Majors <j.a.majors@student.tue.nl>
2019-06-20 16:28:08 +0200

    optional flameshot

--- bin/screenshot.sh
+++ bin/screenshot.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-maim --highlight --color=0.3,0.4,0.6,0.4 -s --format png /dev/stdout | xclip -selection clipboard -t image/png -i
-notify-send -u "low" -t 1000 "Copied screenshot on clipboard!"
+if [ -x "$(command -v flameshot)" ]; then
+    flameshot gui
+else
+    maim --highlight --color=0.3,0.4,0.6,0.4 -s --format png /dev/stdout | xclip -selection clipboard -t image/png -i
+    notify-send -u "low" -t 1000 "Copied screenshot on clipboard!"
+fi
