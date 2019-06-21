54e77689b606bdd57aa51a9a38599832ac518716
Juris Majors <j.a.majors@student.tue.nl>
2019-06-21 18:10:22 +0200

    uplaod and mediainfo preview

--- .config/lf/pv.sh
+++ .config/lf/pv.sh
@@ -4,5 +4,6 @@ case "$1" in
     *.tar*) tar tf "$1";;
     *.zip) unzip - -l "$1";;
     *.pdf) pdftotext "$1" -;;
+    *.mp3 | *.flac | *.ogg | *.mkv | *.mp4 | *.flv | *.mpg | *.mov | *.avi) mediainfo "$1" ;; 
     *) highlight -O ansi "$1" || cat "$1";;
 esac
