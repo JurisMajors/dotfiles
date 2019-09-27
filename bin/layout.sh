718d2f882004b98ec52b138daa34a11e90b3b945
JurisMajors <j.a.majors@student.tue.nl>
2019-09-27 02:12:28 +0200

    Add new layout for new monitor

--- bin/layout.sh
+++ bin/layout.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-output=$(xrandr | grep 'DVI-D-0 connected') 
+output=$(xrandr | grep 'DP-2 connected') 
 
 if [[ $output ]]; then
-    xrandr --output DVI-D-0 --mode 1920x1080 --rate 144 --primary
+    xrandr --output DVI-D-0 --mode 1920x1080 --pos 3840x0 --rotate left --rate 144 --output HDMI-0 --off --output HDMI-1 --off --output DP-0 --off --output DP-1 --off --output DP-2 --primary --mode 3840x2160 --pos 0x0 --rotate normal --rate 60 --output DP-3 --off
 fi
-echo $(feh --randomize --bg-fill ~/Wallpapers/)
